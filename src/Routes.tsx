8de85358598736a56c91332f5bd17e7096ff19ad
Louis <emyhoang8899@gmail.com>
2020-03-05 14:53:16 -0800

    homepage redirect and conditional redirect
    - import redirect from React
    -redirect homepage to /products using exact path: true
    - create login component and use conditiona redirect on admin page based on whether user is logged in or not

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,20 +1,25 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import AdminPage from './AdminPage';
 import ProductsPage from './ProductsPage';
 import Header from './Header';
 import ProductPage from './ProductPage';
 import NotFoundPage from './NotFoundPage';
+import LoginPage from './LoginPage';
 
 const Routes: React.SFC = () => {
+  const [loggedIn, setLoggedIn] = React.useState(true);
+
   return (
     <Router>
       <div>
         <Header />
         <Switch>
+          <Redirect exact={true} from='/' to='/products' />
           <Route exact={true} path='/products' component={ProductsPage} />
           <Route path='/products/:id' component={ProductPage} />
-          <Route path='/admin' component={AdminPage} />
+          <Route path='/admin'>{loggedIn ? <AdminPage /> : <Redirect to='/login' />}</Route>
+          <Route path='/login' component={LoginPage} />
           <Route component={NotFoundPage} />
         </Switch>
       </div>
