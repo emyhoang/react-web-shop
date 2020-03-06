ee8a367d7a628a65f6d17bfcbec4419fee74cb1d
Louis <emyhoang8899@gmail.com>
2020-03-06 13:46:58 -0800

    adding some animation when users navigate to different pages
    - install react-transition-group, @types/react-transition-group --sav-dev
    -import into routes.tsx
    -wrap switch in transition with attributes
    - add  RoutesWrap component and export to fix compilation error in index.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,30 +1,41 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, RouteComponentProps, Switch } from 'react-router-dom';
 import AdminPage from './AdminPage';
 import ProductsPage from './ProductsPage';
 import Header from './Header';
 import ProductPage from './ProductPage';
 import NotFoundPage from './NotFoundPage';
 import LoginPage from './LoginPage';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-const Routes: React.SFC = () => {
-  const [loggedIn, setLoggedIn] = React.useState(true);
-
+const RoutesWrap: React.SFC = () => {
   return (
     <Router>
-      <div>
-        <Header />
-        <Switch>
-          <Redirect exact={true} from='/' to='/products' />
-          <Route exact={true} path='/products' component={ProductsPage} />
-          <Route path='/products/:id' component={ProductPage} />
-          <Route path='/admin'>{loggedIn ? <AdminPage /> : <Redirect to='/login' />}</Route>
-          <Route path='/login' component={LoginPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </div>
+      <Route component={Routes} />
     </Router>
   );
 };
 
-export default Routes;
+const Routes: React.SFC<RouteComponentProps> = props => {
+  const [loggedIn, setLoggedIn] = React.useState(true);
+
+  return (
+    <div>
+      <Header />
+      <TransitionGroup>
+        <CSSTransition key={props.location.key} timeout={500} className='animate'>
+          <Switch>
+            <Redirect exact={true} from='/' to='/products' />
+            <Route exact={true} path='/products' component={ProductsPage} />
+            <Route path='/products/:id' component={ProductPage} />
+            <Route path='/admin'>{loggedIn ? <AdminPage /> : <Redirect to='/login' />}</Route>
+            <Route path='/login' component={LoginPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    </div>
+  );
+};
+
+export default RoutesWrap;
